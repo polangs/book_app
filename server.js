2135930bfc1c421ed50e55075f2459cf797a8033
paulacruz <cruz.amp12@gmail.com>
2019-06-18 13:37:12 -0700

    still working on constructor book

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//added to look into objects
+const util = require('util');
+
 // Application Middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -20,47 +23,48 @@ app.set('view engine', 'ejs');
 app.get('/', (request, response) => {
   response.render('pages/index', {message: 'Woohoo!'})
 });
-
+app.get('/home')
 // Creates a new search to the Google Books API
-// app.post('/searches', createSearch);
+app.post('/searches', createSearch);
 
-// // Catch-all
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
-
-// app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-// HELPER FUNCTIONS
-// Only show part of this to get students started
-// function Book(info) {
-//   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-
-//   this.title = info.title || 'No title available';
-
-// }
+// HELPER FUNCTIONS - constructor/translator
+function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = info.title ? info.title : 'No title available';
+}
 
 // Note that .ejs file extension is not required
-// function newSearch(request, response) {
-//   response.render('pages/index');
-// }
+function newSearch(request, response) {
+  response.render('pages/index');
+}
 
 // No API key required
-// Console.log request.body and request.body.search
-// function createSearch(request, response) {
-//   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+// Console.log (request.body and request.body.search
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-//   console.log(request.body);
-//   console.log(request.body.search);
+  console.log(request.body);
+  console.log(request.body.search);
 
-//   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
-//   if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
 
-// WARNING: won't work as is. Why not?
-// superagent.get(url)
-//   .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-//   .then(results => response.render('pages/searches/show', {searchResults: results}));
-// how will we handle errors?
-// }
+  // WARNING: won't work as is. Why not?
+  // superagent.get(url)
+  //   .then(apiResponse => console.log(util.inspect(apiResponse.body.items, {showhidden: false, depth: null})))
+  //   .then(results => response.render('pages/searches/show', {searchResults: results})
+  //   )}
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', {searchResults: results})
+    )}
 
+// error catcher
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
+
+//error handler
+const handleError = (error, response) => {
+  response.render('pages/error', {error: MSMediaKeyError})
+}
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
