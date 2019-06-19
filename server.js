c2ada8aec1151b2b57dbcf6a9ccaec74972be6d4
paulacruz <cruz.amp12@gmail.com>
2019-06-19 10:53:58 -0700

    book infos are showing, css otw

--- server.js
+++ server.js
@@ -23,25 +23,23 @@ app.set('view engine', 'ejs');
 app.get('/', (request, response) => {
   response.render('pages/index', {message: 'Woohoo!'})
 });
-app.get('/home')
+// app.get('/home')
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
-
 
 
 // HELPER FUNCTIONS - constructor/translator
 function Book(info) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
   this.title = info.title ? info.title : 'No title available';
-  let httpRegex = /^(http: \/\/)/g
+  let httpRegex = /^(http:\/\/)/g;
 
-  // new instances not complete, working on it
-  this.title = info.title ? info.title : 'No title Available';
-  this.author = info.authors ? info.authors[0] : 'No Author Available'; 
-  this.isbn  = info.isbn ? info.isbn
+//   // new instances not complete, working on it
+  this.author = info.authors ? info.authors[0] : 'No Author Available';
+    // this.isbn  = info.isbn ? info.isbn; //edit me  
   this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail.replace(httpRegex, 'http://') : placeholderImage;
-  this.description = info.description ? info.description
-  this.id = info.id ? info.id 
+  this.description = info.description ? info.description: 'No description Available';
+//   this.id = info.id ? info.id
 }
 
 // Note that .ejs file extension is not required
@@ -57,8 +55,8 @@ function createSearch(request, response) {
   console.log(request.body);
   console.log(request.body.search);
 
-  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
-  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`}
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`}
 
   // WARNING: won't work as is. Why not?
   // superagent.get(url)
@@ -67,9 +65,9 @@ function createSearch(request, response) {
   //   )}
   superagent.get(url)
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render('pages/searches/show', {searchResults: results})
-    )}// maybe add catch here
-
+    .then(results => response.render('pages/searches/show', {searchResults: results}))
+    .catch(err => handleError(err, response));
+}
 // error catcher
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
@@ -77,5 +75,4 @@ app.get('*', (request, response) => response.status(404).send('This route does n
 const handleError = (error, response) => {
   response.render('pages/error', {error: error})
 }
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
